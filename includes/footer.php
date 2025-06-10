797a2d626c7659acd4683e2eca0e34368f1e412c
Biniyam <88705089+bzoxx@users.noreply.github.com>
2025-06-10 10:30:20 +0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer>
+        <p>&copy; 2024 Blind Date Hub. All rights reserved.</p>
+        <p><a href="about.php">About</a> | <a href="privacyPolicy.html">Privacy Policy</a> | <a href="terms.html">Terms of Service</a></p>
+    </footer>
+</body>
+</html> 
