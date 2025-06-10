a4199c73a4bc15d2861a8bc6c4bcabbb5a8a9bc1
Biniyam <88705089+bzoxx@users.noreply.github.com>
2025-06-10 10:27:49 +0300

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+// Start the session if it hasn't been started
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
+// Destroy the session
+session_destroy();
+
+// Redirect to login page
+header("Location: login.php");
+exit();
+?> 
